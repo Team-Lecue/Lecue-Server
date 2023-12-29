de951f0bd16e5fdc3606a5b7568d840f5a7b9c87
ddongseop <dlehdtjq00o@gmail.com>
2023-12-30 01:00:55 +0900

    :package: [CHORE] 폴더링 구조 변경 #6

--- src/main/java/org/sopt/lequuServer/global/config/JpaAuditingConfig.java
+++ src/main/java/org/sopt/lequuServer/global/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package org.sopt.lequuServer.global.common.config;
+package org.sopt.lequuServer.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
