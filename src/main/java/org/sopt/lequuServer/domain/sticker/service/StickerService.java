86308afc1b874233fe11eb7f4b4b5221d719295a
ddongseop <dlehdtjq00o@gmail.com>
2024-01-06 16:42:18 +0900

    ✏️[FIX] 불필요한 검증 로직 삭제 #26

--- src/main/java/org/sopt/lequuServer/domain/sticker/service/StickerService.java
+++ src/main/java/org/sopt/lequuServer/domain/sticker/service/StickerService.java
@@ -1,12 +1,9 @@
 package org.sopt.lequuServer.domain.sticker.service;
 
 import lombok.RequiredArgsConstructor;
-import org.sopt.lequuServer.domain.book.repository.BookJpaRepository;
 import org.sopt.lequuServer.domain.sticker.dto.response.StickerPackResponse;
 import org.sopt.lequuServer.domain.sticker.model.Sticker;
 import org.sopt.lequuServer.domain.sticker.repository.StickerJpaRepository;
-import org.sopt.lequuServer.global.exception.enums.ErrorType;
-import org.sopt.lequuServer.global.exception.model.CustomException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,19 +15,14 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class StickerService {
 
-    private final BookJpaRepository bookRepository;
     private final StickerJpaRepository stickerRepository;
 
     public List<StickerPackResponse> getStickerPackList(Long bookId) {
 
-        if (!bookRepository.existsById(bookId)) {
-            throw new CustomException(ErrorType.NOT_FOUND_BOOK_ERROR);
-        }
-
         // 공통 스티커팩 + 해당 레큐북 스티커팩
         List<Long> bookIds = Arrays.asList(0L, bookId);
         List<Sticker> stickerPack = stickerRepository.findStickersByBookIds(bookIds);
-        
+
         return StickerPackResponse.of(stickerPack);
     }
 }
