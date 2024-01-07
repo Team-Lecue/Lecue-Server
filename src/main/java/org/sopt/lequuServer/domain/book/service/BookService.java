7bf024974c3f1a53aa33c57b38fc25292a189d06
dong2ast <kdh01132006@naver.com>
2024-01-07 12:21:21 +0900

    ♻️ [REAFCTOR] 코드 스타일 변경

--- src/main/java/org/sopt/lequuServer/domain/book/service/BookService.java
+++ src/main/java/org/sopt/lequuServer/domain/book/service/BookService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class BookService {
+
     private final BookRepository bookRepository;
 
     public Book getBookById(Long id) {
