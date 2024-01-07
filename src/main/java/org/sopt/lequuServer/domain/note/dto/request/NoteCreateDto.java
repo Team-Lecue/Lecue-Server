0ae065c56e8de2265f553a0d83d4506e0e98b21b
dong2ast <kdh01132006@naver.com>
2024-01-07 20:14:20 +0900

    ♻️ [REFACTOR] PR 리뷰 반영

--- src/main/java/org/sopt/lequuServer/domain/note/dto/request/NoteCreateDto.java
+++ src/main/java/org/sopt/lequuServer/domain/note/dto/request/NoteCreateDto.java
@@ -1,6 +1,8 @@
 package org.sopt.lequuServer.domain.note.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record NoteCreateDto(
         @Schema(example = "1")
@@ -8,7 +10,10 @@ public record NoteCreateDto(
         //TODO DB상에서 관리하려면 그냥 Auditing으로 하는게 좋을지도?
         @Schema(example = "2023.12.27 (수)")
         String noteDate,
+        //TODO validation 추가
         @Schema(example = "test")
+        @NotBlank
+        @Size(min = 1, max = 1000, message = "레큐노트 내용은 1자 이상 1000자 이하여야합니다.")
         String content,
         @Schema(example = "0")
         int textColor,
