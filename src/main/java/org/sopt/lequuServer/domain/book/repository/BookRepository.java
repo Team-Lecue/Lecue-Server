889b771a61b109636a03e6cb90ac1547c852cac7
dong2ast <kdh01132006@naver.com>
2024-01-07 12:15:12 +0900

    ✨ [FEAT] id로 Book 조회하는 기능 구현

--- src/main/java/org/sopt/lequuServer/domain/book/repository/BookRepository.java
+++ src/main/java/org/sopt/lequuServer/domain/book/repository/BookRepository.java
@@ -1,7 +1,14 @@
 package org.sopt.lequuServer.domain.book.repository;
 
 import org.sopt.lequuServer.domain.book.model.Book;
+import org.sopt.lequuServer.global.exception.enums.ErrorType;
+import org.sopt.lequuServer.global.exception.model.CustomException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    default Book getBookById(Long id) {
+        return this.findById(id).orElseThrow(
+                () -> new CustomException(ErrorType.NOT_FOUND_BOOK_ERROR));
+    }
 }
---
+++ src/main/java/org/sopt/lequuServer/domain/book/service/BookService.java
@@ -0,0 +1,16 @@
+package org.sopt.lequuServer.domain.book.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.lequuServer.domain.book.model.Book;
+import org.sopt.lequuServer.domain.book.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public Book getBookById(Long id) {
+        return bookRepository.getBookById(id);
+    }
+}
