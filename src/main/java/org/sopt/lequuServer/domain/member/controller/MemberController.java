b3f807072e14c1abe11376ea3bd5bbb263554e86
eeddiinn <gdw4013@naver.com>
2024-01-09 16:34:48 +0900

    🐛 [FIX] 최애 이름, 레큐북 제목, 레큐북 소개 글자 수 제한 수정 #14

--- src/main/java/org/sopt/lequuServer/domain/member/controller/MemberController.java
+++ src/main/java/org/sopt/lequuServer/domain/member/controller/MemberController.java
@@ -8,12 +8,11 @@ import org.sopt.lequuServer.global.auth.fegin.kakao.KakaoLoginService;
 import org.sopt.lequuServer.global.auth.jwt.JwtProvider;
 import org.sopt.lequuServer.global.auth.jwt.TokenDto;
 import org.sopt.lequuServer.global.common.dto.ApiResponse;
+import org.sopt.lequuServer.global.exception.enums.SuccessType;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-
-import static org.sopt.lequuServer.global.exception.enums.SuccessType.*;
 
 @RestController
 @RequestMapping("/api")
@@ -29,7 +28,7 @@ public class MemberController {
             @RequestHeader("Authorization") String socialAccessToken,
             @RequestBody SocialLoginRequestDto request) {
 
-        return ApiResponse.success(LOGIN_SUCCESS, memberService.login(socialAccessToken, request));
+        return ApiResponse.success(SuccessType.LOGIN_SUCCESS, memberService.login(socialAccessToken, request));
     }
 
     @GetMapping("/reissue")
@@ -37,7 +36,7 @@ public class MemberController {
     public ApiResponse<TokenDto> reissue(
             @RequestHeader("Authorization") String refreshToken) {
 
-        return ApiResponse.success(REISSUE_SUCCESS, memberService.reissueToken(refreshToken));
+        return ApiResponse.success(SuccessType.REISSUE_SUCCESS, memberService.reissueToken(refreshToken));
     }
 
     @PatchMapping("/log-out") // Spring Security 자체의 logout과 겹치지 않기 위해 이렇게 설정
@@ -45,7 +44,7 @@ public class MemberController {
     public ApiResponse<?> logout(Principal principal) {
 
         memberService.logout(JwtProvider.getUserFromPrincial(principal));
-        return ApiResponse.success(LOGOUT_SUCCESS);
+        return ApiResponse.success(SuccessType.LOGOUT_SUCCESS);
     }
 
     @GetMapping("/kakao")
@@ -53,6 +52,6 @@ public class MemberController {
     public ApiResponse<?> kakaoAccessToken(
             @RequestHeader("Authorization") String code) {
 
-        return ApiResponse.success(KAKAO_ACCESS_TOKEN_SUCCESS, kakaoLoginService.getKakaoAccessToken(code));
+        return ApiResponse.success(SuccessType.KAKAO_ACCESS_TOKEN_SUCCESS, kakaoLoginService.getKakaoAccessToken(code));
     }
 }
