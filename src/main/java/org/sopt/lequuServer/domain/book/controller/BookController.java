f47fbf431418ee846b093f4d7a089e17caa6087d
dong2ast <kdh01132006@naver.com>
2024-01-09 17:16:19 +0900

    📝 [MEMO] Swagger summary 변경

--- src/main/java/org/sopt/lequuServer/domain/book/controller/BookController.java
+++ src/main/java/org/sopt/lequuServer/domain/book/controller/BookController.java
@@ -27,13 +27,13 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(description = "레큐 북 생성")
+    @Operation(summary = "레큐 북 생성")
     public ApiResponse<BookCreateResponseDto> createBook(@Valid @RequestBody BookCreateRequestDto request, Principal principal) {
         return ApiResponse.success(SuccessType.BOOK_CREATE_SUCCESS, bookFacade.createBook(request, JwtProvider.getUserFromPrincial(principal)));
     }
 
     @DeleteMapping("/{bookId}")
-    @Operation(description = "레큐 북 삭제")
+    @Operation(summary = "레큐 북 삭제")
     public ApiResponse<?> deleteBook(@PathVariable Long bookId) {
         bookFacade.deleteBook(bookId);
         return ApiResponse.success(SuccessType.BOOK_DELETE_SUCCESS);
