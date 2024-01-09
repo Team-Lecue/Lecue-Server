512f26fa98aa12b46004696117feff8e6297cee9
ddongseop <dlehdtjq00o@gmail.com>
2024-01-10 03:11:41 +0900

    :memo: [DOCS] CORS allowedOrigins 추가

--- src/main/java/org/sopt/lequuServer/global/config/WebConfig.java
+++ src/main/java/org/sopt/lequuServer/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080")
+                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
