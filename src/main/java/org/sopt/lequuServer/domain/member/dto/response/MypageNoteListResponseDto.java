bcb2896d9c2a360a194e923dd3841d4aa18241f5
eeddiinn <gdw4013@naver.com>
2024-01-10 16:01:20 +0900

    ✏️ [FIX] 레큐노트 생성 일시 날짜 형식 수정 #11

--- src/main/java/org/sopt/lequuServer/domain/member/dto/response/MypageNoteListResponseDto.java
+++ src/main/java/org/sopt/lequuServer/domain/member/dto/response/MypageNoteListResponseDto.java
@@ -3,24 +3,27 @@ package org.sopt.lequuServer.domain.member.dto.response;
 import org.sopt.lequuServer.domain.note.model.Note;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record MypageNoteListResponseDto(
         String bookUuid,
         String title,
         Long noteId,
         String content,
-        LocalDateTime noteDate,
+        String noteDate,
         int noteBackgroundColor,
         String noteBackgroundImage
 ) {
     public static MypageNoteListResponseDto of(Note note) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        String noteDate = note.getCreatedAt().format(formatter);
 
         return new MypageNoteListResponseDto(
                 note.getBook().getUuid(),
                 note.getBook().getTitle(),
                 note.getId(),
                 note.getContent(),
-                note.getCreatedAt(),
+                noteDate,
                 note.getBackgroundColor(),
                 note.getBackgroundImage()
         );
--- src/main/java/org/sopt/lequuServer/domain/member/dto/response/MypageNoteResponseDto.java
+++ src/main/java/org/sopt/lequuServer/domain/member/dto/response/MypageNoteResponseDto.java
@@ -2,6 +2,7 @@ package org.sopt.lequuServer.domain.member.dto.response;
 
 import org.sopt.lequuServer.domain.note.model.Note;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,15 +12,19 @@ public record MypageNoteResponseDto(
 ) {
     public static MypageNoteResponseDto of(List<Note> notes) {
         List<MypageNoteListResponseDto> mypageNoteList = notes.stream()
-                .map(note -> new MypageNoteListResponseDto(
-                        note.getBook().getUuid(),
-                        note.getBook().getTitle(),
-                        note.getId(),
-                        note.getContent(),
-                        note.getCreatedAt(),
-                        note.getBackgroundColor(),
-                        note.getBackgroundImage()
-                ))
+                .map(note -> {
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+                    String noteDate = note.getCreatedAt().format(formatter);
+                    return new MypageNoteListResponseDto(
+                            note.getBook().getUuid(),
+                            note.getBook().getTitle(),
+                            note.getId(),
+                            note.getContent(),
+                            noteDate,
+                            note.getBackgroundColor(),
+                            note.getBackgroundImage()
+                    );
+                })
                 .collect(Collectors.toList());
 
         return new MypageNoteResponseDto(mypageNoteList);
