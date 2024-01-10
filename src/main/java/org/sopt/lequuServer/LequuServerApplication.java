82c35370748d2fdfff0115a9c395acf148ac817a
dong2ast <kdh01132006@naver.com>
2024-01-10 12:15:51 +0900

    📦️ [CHORE] 스웨거 https 설정

--- src/main/java/org/sopt/lequuServer/LequuServerApplication.java
+++ src/main/java/org/sopt/lequuServer/LequuServerApplication.java
@@ -1,14 +1,11 @@
 package org.sopt.lequuServer;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
-@OpenAPIDefinition(servers = {@Server(url ="/", description = "Default Server URL")})
 @EnableFeignClients
 public class LequuServerApplication {
 
