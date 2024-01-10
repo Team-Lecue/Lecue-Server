649d5ae2da8d54b3c9dc5e10fbf7a957462f8246
ddongseop <dlehdtjq00o@gmail.com>
2024-01-11 02:09:23 +0900

    :twisted_rightwards_arrows: [MERGE] merge 오류 해결

--- src/main/java/org/sopt/lequuServer/global/common/model/BaseTimeEntity.java
+++ src/main/java/org/sopt/lequuServer/global/common/model/BaseTimeEntity.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@Getter
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 public abstract class BaseTimeEntity {
