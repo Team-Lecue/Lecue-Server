0df6d1eb362d61b8d6b572a675a64b55272669b6
dong2ast <kdh01132006@naver.com>
2024-01-12 11:27:30 +0900

    📦️ [CHORE] localhost origin 추가

--- src/main/java/org/sopt/lequuServer/global/config/WebConfig.java
+++ src/main/java/org/sopt/lequuServer/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "http://localhost:3000")
+                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "http://localhost:3000", "http://localhost:3001")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
