9e663d62970479f5c5d85f03b993ccc8707250a0
ddongseop <dlehdtjq00o@gmail.com>
2024-01-12 15:01:56 +0900

    :pencil2: [FIX] 닉네임 스페이스바 허용 #46

--- src/main/java/org/sopt/lequuServer/domain/member/dto/request/MemberNicknameRequestDto.java
+++ src/main/java/org/sopt/lequuServer/domain/member/dto/request/MemberNicknameRequestDto.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 public record MemberNicknameRequestDto(
         @NotBlank
         @Size(min = 2, max = 8, message = "닉네임은 2글자 이상 8글자 이하여야합니다.")
-        @Pattern(regexp = "^[A-Za-z0-9가-힣]+$", message = "닉네임은 한글, 영문 대소문자, 숫자만 가능합니다.")
+        @Pattern(regexp = "^[A-Za-z0-9가-힣\\s]+$", message = "닉네임은 한글, 영문 대소문자, 숫자만 가능합니다.")
         String nickname
 ) {
 }
