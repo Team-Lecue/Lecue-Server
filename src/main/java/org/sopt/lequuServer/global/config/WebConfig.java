2a1e3523f628062d714e7703c6e5c7c601892f4f
Dongseop Lee <67463603+ddongseop@users.noreply.github.com>
2024-01-14 14:22:49 +0900

    :memo: [DOCS] Update WebConfig.java

--- src/main/java/org/sopt/lequuServer/global/config/WebConfig.java
+++ src/main/java/org/sopt/lequuServer/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "http://localhost:3000", "http://localhost:3001", "https://lecue-client.vercel.app")
+                .allowedOrigins("http://localhost:8080", "http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost:3001", "https://lecue-client.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
