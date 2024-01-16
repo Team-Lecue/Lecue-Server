c041c383d2d9034fa3d46b9e9478d5bbdc1bd4ba
ddongseop <dlehdtjq00o@gmail.com>
2024-01-17 06:42:51 +0900

    :bug: [FIX] CORS 오류 해결

--- src/main/java/org/sopt/lequuServer/global/config/SecurityConfig.java
+++ src/main/java/org/sopt/lequuServer/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -51,8 +52,14 @@ public class SecurityConfig {
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-
-        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/v3/api-docs", config);
         return new CorsFilter(source);
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(
+                "/swagger-ui/**", "/swagger-resources/**", "/api-docs/**",
+                "/v3/api-docs/**", "/webjars/**");
     }
 }
