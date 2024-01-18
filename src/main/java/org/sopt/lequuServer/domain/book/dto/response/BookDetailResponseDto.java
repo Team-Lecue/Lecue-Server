c29fa8ad7c2e8a8c8498b8348b7b4d721a3aecbf
dong2ast <kdh01132006@naver.com>
2024-01-19 01:24:29 +0900

    🐛 [FIX] renderType을 가장 오래된 것부터 1로 count => 새로 추가될때마다 기존 노트의 타입이 바뀌는 오류 해결

--- src/main/java/org/sopt/lequuServer/domain/book/dto/response/BookDetailResponseDto.java
+++ src/main/java/org/sopt/lequuServer/domain/book/dto/response/BookDetailResponseDto.java
@@ -1,18 +1,16 @@
 package org.sopt.lequuServer.domain.book.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.sopt.lequuServer.domain.book.model.Book;
 import org.sopt.lequuServer.domain.note.dto.response.NoteDetailResponseDto;
 import org.sopt.lequuServer.domain.note.model.Note;
 import org.sopt.lequuServer.domain.sticker.dto.response.PostedStickerDetailResponseDto;
 import org.sopt.lequuServer.domain.sticker.model.PostedSticker;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-
-import static java.util.Comparator.comparing;
 
 public record BookDetailResponseDto(
 
@@ -50,18 +48,14 @@ public record BookDetailResponseDto(
     public static BookDetailResponseDto of(Book book) {
         String bookDate = formatLocalDate(book);
 
-        // 레큐노트 리스트를 noteId 기준 내림차순으로 정렬
-        List<Note> sortedNotes = book.getNotes().stream()
-                .sorted(comparing(Note::getCreatedAt).reversed())
-                .toList();
-
         // 레큐노트 리스트 가공
         int renderTypeCounter = 1;
         List<NoteDetailResponseDto> noteList = new ArrayList<>();
-        for (Note note : sortedNotes) {
+        for (Note note : book.getNotes()) {
             noteList.add(NoteDetailResponseDto.of(note, renderTypeCounter));
             renderTypeCounter = (renderTypeCounter % 6 == 0) ? 1 : renderTypeCounter + 1;
         }
+        Collections.reverse(noteList);
 
         // 부착된 스티커 리스트 가공
         List<PostedSticker> postedStickers = book.getPostedStickers();
