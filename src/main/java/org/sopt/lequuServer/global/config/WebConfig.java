3869d0bde1f307765048f14cff0350d7c76fdc78
ddongseop <dlehdtjq00o@gmail.com>
2024-02-22 13:58:34 +0900

    :package: [CHORE] CORS 정책 변경

--- src/main/java/org/sopt/lequuServer/global/config/WebConfig.java
+++ src/main/java/org/sopt/lequuServer/global/config/WebConfig.java
@@ -10,9 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost:3001", "https://lecue-client.vercel.app", "https://www.lecue.me")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowCredentials(true)
+//                .allowedOrigins("http://localhost:8080", "http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost:3001", "https://lecue-client.vercel.app", "https://www.lecue.me")
+                .allowedOrigins("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
                 .maxAge(3000);
     }
 }
